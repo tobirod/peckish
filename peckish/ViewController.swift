6ee95df0afcc71a113e9f9d9c33dd0fa43232b0e
tobirod <tobias.rodeback@gmail.com>
2017-09-14 13:42:57 +0200

    no message

--- peckish/ViewController.swift
+++ peckish/ViewController.swift
@@ -16,12 +16,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+        
         tableView.tableFooterView = UIView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addTapped() {
+        
+        print("bubba")
+        
     }
 
 
