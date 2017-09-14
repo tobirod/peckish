c28a0404b32f7f02b21dfc75e3474bc7472434a5
tobirod <tobias.rodeback@gmail.com>
2017-09-14 13:13:22 +0200

    Background added Logo added TableView added

--- peckish/ViewController.swift
+++ peckish/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.tableFooterView = UIView()
     }
 
     override func didReceiveMemoryWarning() {
